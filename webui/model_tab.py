b7ad7782d3c3b8d13ab1c542306c3f045e911482
Ahmad Mahmoud <ahmad2002bc@gmail.com>
2026-09-10 00:08:23 +0300

    fix(webui): make _select_block event arg optional (Gradio 6 passes 2 args)

--- webui/model_tab.py
+++ webui/model_tab.py
@@ -70,9 +70,12 @@ def _detail_md(blocks, bid, tech):
     return "\n".join(lines)
 
 
-def _select_block(blocks, tech, evt):
-    idx = evt.index if not isinstance(evt.index, (list, tuple)) else evt.index[0]
-    bid = blocks[min(int(idx), len(blocks) - 1)]["id"]
+def _select_block(blocks, tech, evt=None):
+    if evt is None:
+        bid = blocks[0]["id"] if blocks else ""
+    else:
+        idx = evt.index if not isinstance(evt.index, (list, tuple)) else evt.index[0]
+        bid = blocks[min(int(idx), len(blocks) - 1)]["id"]
     return _detail_md(blocks, bid, bool(tech))
 
 
